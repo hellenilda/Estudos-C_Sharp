c2ebd84e4464b13c51e39895fbd6100bdea6a4bf
hellenilda <hellen95limaaraujo@gmail.com>
2023-03-21 20:39:53 -0300

    1rst commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+
+namespace PrimeiroPrograma
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Console.WriteLine("Hello, World!");
+            Console.Beep(); //Faz um Beep ao abrir o console
+            */
+
+            int x; //primeira forma de declaração
+            x = 234;
+
+            int y = 343; //segunda forma de declaração
+            String nome = "Hellen";
+            double altura = 1.64; //double
+            bool condicao = true; //booleano
+            
+            Console.WriteLine(x+" e "+y);
+            Console.WriteLine("Altura: "+altura);
+            Console.WriteLine("Nome: "+nome);
+            Console.WriteLine("Condição booleana: "+condicao);
+
+
+            Console.ReadKey();
+        }
+    }
+}
