c31b3c7aa826b47e864cb3030f087265e854de3c
hellenilda <hellen95limaaraujo@gmail.com>
2023-03-26 11:21:41 -0300

    Else-If

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,39 +1,20 @@
-﻿//using System;
-
-namespace PrimeiroPrograma
+﻿namespace PrimeiroPrograma
 {
     class HelloWorld
     {
         static void Main(string[] args)
         {
-            /*
-             * Console.WriteLine("Hello, World!");
-            Console.Beep(); //Faz um Beep ao abrir o 
+            Console.WriteLine("Qual sua idade? ");
+            int idade = Convert.ToInt32(Console.ReadLine());
 
-            int x; //primeira forma de declaração
-            x = 234;
-
-            int y = 343; //segunda forma de declaração
-            String nome = "Hellen";
-            double altura = 1.64; //double
-            bool condicao = true; //booleano
-            
-            Console.WriteLine(x+" e "+y);
-            Console.WriteLine("Altura: "+altura);
-            Console.WriteLine("Nome: "+nome);
-            Console.WriteLine("Condição booleana: "+condicao
-            */
-
-            int x = 16;
-            int y = -3;
-
-            double exponenciacao = Math.Pow(x, 2);
-            double raiz = Math.Sqrt(x);
-            double modulo = Math.Abs(y);
-
-            Console.WriteLine(exponenciacao);
-            Console.WriteLine(raiz);
-            Console.WriteLine(modulo);
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você é de maior!");
+            }
+            else
+            {
+                Console.WriteLine("Você é menor de idade!");
+            }
 
             Console.ReadKey();
         }
